aad8fa2100f49b10b245741f8710b5ed51a8b403
Hassan264 <hassan.al.masri.mail@gmail.com>
2024-04-18 15:20:04 +0300

    prepare the store

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,37 @@
 import { createStore } from 'vuex'
 
-export default createStore({
-    state: {},
-    getters: {},
-    mutations: {},
-    actions: {},
-    modules: {},
+const store = createStore({
+    state: {
+        movies: [], // Array to store movies
+    },
+    mutations: {
+        addMovie(state, movie) {
+            state.movies.push(movie)
+        },
+        updateMovie(state, { index, movie }) {
+            state.movies[index] = movie
+        },
+        deleteMovie(state, index) {
+            state.movies.splice(index, 1)
+        },
+        // getMovies(state) {
+        //     // get movie from local storage
+        // },
+    },
+    actions: {
+        addMovie({ commit }, movie) {
+            commit('addMovie', movie)
+        },
+        updateMovie({ commit }, payload) {
+            commit('updateMovie', payload)
+        },
+        deleteMovie({ commit }, index) {
+            commit('deleteMovie', index)
+        },
+        // getMovies({ commit }) {
+        //     commit('loadMovies')
+        // },
+    },
 })
+
+export default store
